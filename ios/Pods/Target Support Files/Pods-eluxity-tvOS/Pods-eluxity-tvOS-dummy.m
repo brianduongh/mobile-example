618720df15a227a9314912e1bab9f1bdb16f2b46
Ardhimas Kamdani <kamdani.af@gmail.com>
2018-05-10 15:35:27 +0700

    updated gitignore to ignore Pods folder, updated Podfile to lock Firebase version

--- ios/Pods/Target Support Files/Pods-eluxity-tvOS/Pods-eluxity-tvOS-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_eluxity_tvOS : NSObject
-@end
-@implementation PodsDummy_Pods_eluxity_tvOS
-@end
--- ios/Pods/Target Support Files/Pods-eluxityTests/Pods-eluxityTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_eluxityTests : NSObject
-@end
-@implementation PodsDummy_Pods_eluxityTests
-@end
