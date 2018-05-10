6c1b352319a60847143e56e6e0ebce859540a4ff
Ardhimas Kamdani <kamdani.af@gmail.com>
2018-05-10 15:36:52 +0700

    added gitignore and App.js

--- App.js
+++ App.js
@@ -1,13 +1,34 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { View, Text } from 'react-native';
 import firebase from 'react-native-firebase';
 
-class App extends Component {
+class App extends React.Component {
+
+  constructor() {
+    super();
+    this.state = {
+      isAuthenticated: false,
+    };
+  }
+
+  componentDidMount() {
+    firebase.auth().signInAnonymously()
+      .then(() => {
+        this.setState({
+          isAuthenticated: true,
+        });
+      });
+  }
 
   render() {
+    // If the user has not authenticated
+    if (!this.state.isAuthenticated) {
+      return <View><Text>Not signed in</Text></View>;
+    }
+
     return (
       <View>
-        <Text>Welcome to my awesome app!</Text>
+        <Text>Welcome to my awesfdfd</Text>
       </View>
     );
   }
